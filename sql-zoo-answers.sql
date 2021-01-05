2feeb1b61992eb46270e0802b1a0c7ff28608749
helciodev <helcioandre@live.com>
2021-01-05 15:49:33 +0100

    finishes answer for NULL and CASE operations

--- sql-zoo-answers.sql
+++ sql-zoo-answers.sql
@@ -419,3 +419,65 @@ SELECT title, name FROM actor act, movie mov
 JOIN casting cas ON ( cas.movieid = mov.id) WHERE mov.yr = 1962
 AND act.id = cas.actorid
 AND cas.ord = 1
+
+-- Using Null
+-- https://sqlzoo.net/wiki/Using_Null
+-- Teachers and Departments
+
+-- 1. List the teachers who have NULL for their department.
+-- Answer:
+SELECT name FROM teacher WHERE dpt is NULL
+
+-- 2 Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+-- Answer
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+      
+-- 3. Use a different JOIN so that all teachers are listed.
+-- Answer:
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- 4. Use a different JOIN so that all departments are listed.
+-- Answer:
+SELECT teacher.name, dept.name
+ FROM teacher right JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- 5. Using the COALESCE function
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+-- Answer
+SELECT name, coalesce(mobile, '07986 444 2266') AS mobile FROM teacher 
+
+-- 6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+-- Answer:
+SELECT teacher.name, COALESCE(dept.name, 'None')
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- 7. Use COUNT to show the number of teachers and the number of mobile phones.
+-- Answer:
+SELECT COUNT( name ) AS teachers, COUNT(mobile) AS mobile FROM teacher
+
+-- 8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+-- Answer:
+SELECT dept.name, COUNT(teacher.name) AS staff_number FROM teacher
+RIGHT JOIN dept ON (teacher.dept = dept.id) GROUP BY dept.name
+
+-- 9. Using CASE
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+-- Answer
+SELECT name, CASE WHEN dept = 1 THEN 'Sci'
+                  WHEN dept = 2 THEN 'Sci'
+                  ELSE 'Art'
+             END AS subject FROM teacher
+
+-- 10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+-- Answer:
+SELECT name, CASE WHEN dept = 1 THEN 'Sci'
+                  WHEN dept = 2 THEN 'Sci'
+                  WHEN dept = 3 THEN 'Sci'
+                  ELSE 'None'
+             END as subject FROM teacher
